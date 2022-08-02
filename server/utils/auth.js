dae0d69af426ee096626fb6262528ecca3d52fc6
Vue Lee <vuelee09@gmail.com>
2022-08-02 17:06:12 -0500

    update auth middleware function to work with GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,31 +6,34 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
+  authMiddleware: function({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+  
+    // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+        token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
-
+  
+    // if no token, return request object as is
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+        return req;
     }
-
-    // verify token and get user data out of it
+  
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+        // decode and attach user data to request object
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+        console.log('Invalid token');
     }
-
-    // send to next endpoint
-    next();
-  },
+  
+    // return updated request object
+    return req;
+},
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
