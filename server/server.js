a5963025ce0d1f69c09be59fbf2f582d00326207
Vue Lee <vuelee09@gmail.com>
2022-08-02 17:52:29 -0500

    update server file

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { ApolloServer } = require('apollo-server-express'); // import ApolloServer
+const { typeDefs, resolvers } = require('./schemas'); // import our typeDefs and resolvers
+const { authMiddleware } = require('./utils/auth'); // import middleware
+
+const server = new ApolloServer({ // create a new Apollo server and pass in our schema data
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.use(routes);
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
+  // Serve up static assets
+  // check to see if the Node environment is in production
+  if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+
+  db.once('open', () => {
+      app.listen(PORT, () => {
+          console.log(`API server running on port ${PORT}!`);
+          // log where we can go to test our GQL API
+          console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
